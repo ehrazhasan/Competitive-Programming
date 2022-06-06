3de151766c41fea1d45ab224d0db0943afbb144c
ehrazhasan <ehraz_hasan@outlook.com>
2022-06-06 16:38:06 +0530

    Upsolve

---
+++ Codechef/Codechef Starters 41/E.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h> 
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+//using namespace __gnu_pbds;
+ 
+typedef long long ll;
+typedef long double ld;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define db1(x) cout<<#x<<"="<<x<<"\n"
+#define db2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<'\n'
+#define db3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<'\n'
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define repA(i,a,n) for(int i=a;i<=(n);++i)
+#define repD(i,a,n) for(int i=a;i>=(n);--i)
+#define MOD 1000000007
+#define gcd(a,b) __gcd(a,b)
+#define tolowecase(s1) transform(s1.begin(),s1.end(),s1.begin(), ::tolower)
+#define touppercase(s1) transform(s1.begin(),s1.end(),s1.begin(), ::toupper)
+#define MOD1 998244353
+#define eps 1e-12
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define PI 3.141592653589793238462
+#define total_set_bits __builtin_popcountll
+
+
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3 // return x, y, gcd
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+    
+                /*******************************************************************************/
+
+const ll inf = 1e12;
+const int N = 3e5 + 5;
+ll n;
+vector<int> edges[N];
+vector<int> a, b;
+vector<vector<ll>> dp;
+
+void dfs(int node, int parent){
+
+    if(b[node] == 1) dp[node][1]++;
+    else dp[node][0]++;
+
+    if(a[node] != b[node]){
+        
+        dp[node][2] = inf;
+    }
+
+    for(auto nbr : edges[node]){
+
+        if(nbr == parent) continue;
+
+        dfs(nbr, node);
+        if(b[node] == 0){
+
+            dp[node][0] += dp[nbr][1];
+            dp[node][1] += dp[nbr][1];
+        }
+        else{
+
+            dp[node][0] += dp[nbr][0];
+            dp[node][1] += dp[nbr][0];
+        }
+        // dp[node][0] += dp[nbr][0];
+        // dp[node][1] += dp[nbr][1];
+        dp[node][2] += min(dp[nbr][2], min(1 + dp[nbr][1], 1 + dp[nbr][0]));
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        
+        cin >> n;
+        a.assign(n, 0);
+        b.assign(n, 0);
+
+        rep(i,n) cin >> a[i];
+        rep(i,n) cin >> b[i];
+
+        rep(i,n + 2) edges[i].clear();
+        
+        rep(i, n - 1){
+
+            int u, v;
+            cin >> u >> v;
+
+            u--; v--;
+
+            edges[u].push_back(v);
+            edges[v].push_back(u);
+        }
+
+        dp = vector<vector<ll>>(n + 1, vector<ll>(3, 0));
+
+        dfs(0, -1);
+
+        cout << min(dp[0][2], min(1 + dp[0][0], 1 + dp[0][1])) << '\n';
+    }
+    return 0;
+}
